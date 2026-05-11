d8f7dfc7ed5ba732183ab76f6ad33e512fa0ab46
xiaohu <duonght2004.itsgu@gmail.com>
2026-05-11 22:42:41 +0700

    fix error call api

--- app/(dashboard)/admin/topics/page.tsx
+++ app/(dashboard)/admin/topics/page.tsx
@@ -1,11 +1,6 @@
-import TopicsTable from "@/app/(dashboard)/admin/_components/topics-table";
-import apiFetch from "@/lib/api/api-fetch";
-import { ApiPaginationResponse } from "@/lib/types/api";
-import { TopicType } from "@/lib/types/topic";
 
-export default async function TopicsPage() {
-    const dataTopics = await apiFetch.get<ApiPaginationResponse<TopicType[]>>("/topics");
+export default function TopicsPage() {
     return <div>
-          <TopicsTable pagination={dataTopics.pagination} initialData={dataTopics.data} />
+        topic page
     </div>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export default function HomePage() {
-     const router = useRouter()
-     useEffect(()=>{
-           router.push("/admin")
-     },[])
-    return <div>
-          
-     </div>
+      const router = useRouter()
+      useEffect(() => {
+            router.push("/admin")
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [])
+      return <div>
+
+      </div>
 }
